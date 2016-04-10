eacf5e3f0f80d8d43b935d9759475489ab69df18
alysondantas <alyson.lipe@gmail.com>
2016-04-09 23:01:33 -0300

    Criando classes

---
+++ src/main/java/alysondantas/agendador/controller/controller.java
@@ -0,0 +1,5 @@
+package main.java.alysondantas.agendador.controller;
+
+public class controller {
+
+}
---
+++ src/main/java/alysondantas/agendador/model/Desligar.java
@@ -0,0 +1,11 @@
+package main.java.alysondantas.agendador.model;
+
+public class Desligar {
+	public Desligar(){
+		
+	}
+	public boolean deligarPorHora(int hora){
+		
+		return false;
+	}
+}
